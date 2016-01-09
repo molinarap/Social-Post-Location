8bcec7ebe97928494729c23e1de83f56dda6e7c3
Paolo Molinara <pmolinara@gmail.com>
2016-01-09 18:09:32 +0100

    creato l'endpoint er le api

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,11 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
+
+var router = express.Router();  
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
