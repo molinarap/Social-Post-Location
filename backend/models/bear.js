31ab816e9db5dae56ccccc34ab71b4a8d24424f9
Paolo Molinara <pmolinara@gmail.com>
2016-01-09 18:10:23 +0100

    primo modello di prova con mongoose

---
+++ backend/models/bear.js
@@ -0,0 +1,15 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var BearSchema   = new Schema({
+    name: {
+    	type: String,
+    	required: true
+    },
+    surname: {
+    	type: String,
+    	required: true
+    }
+});
+
+module.exports = mongoose.model('Bear', BearSchema);
