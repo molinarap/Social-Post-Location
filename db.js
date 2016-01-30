868064fdeba89243c39f98a1b58655ad039598a4
Paolo Molinara <pmolinara@gmail.com>
2016-01-30 13:03:20 +0100

    gestione db fuori da app.js

---
+++ db.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var config = require('./config');
+
+mongoose.connect(config.db);
+
+mongoose.connection.on('error', function(err) {
+    console.log('db', err);
+    //mongoose.disconnect();
+});
+
+mongoose.connection.close(function() {
+    console.log('Mongoose disconnected on app termination');
+});
+
