9322ef25562a36c8bd7034dfc8cb314d2c159c68
Paolo Molinara <pmolinara@gmail.com>
2016-03-06 12:24:39 +0100

    client_id Instagram vecchie api

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
     //db: process.env.db || 'mongodb://23.97.199.59:27017/social-db',
     db: process.env.db || 'mongodb://127.0.0.1:27017/social-db',
     clientSecret: process.env.clientSecret || 'a18c44ad660240dda4e2af1fbf0a5abf',
-    tokenSecret: process.env.tokenSecret || 'socialPostApp'
+    tokenSecret: process.env.tokenSecret || 'socialPostApp',
+    client_id_old: '0fbcf8f88e6d45b89ae445fd961a752f'
 };
