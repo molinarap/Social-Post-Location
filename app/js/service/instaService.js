1c1dae386415928e5ed26d1a3b86887812e0916d
Paolo Molinara <pmolinara@gmail.com>
2016-04-30 11:05:00 +0200

    Refactory dei service angolar di app

--- app/js/service/instaService.js
+++ app/js/service/instaService.js
@@ -2,12 +2,12 @@ socialApp
     .factory('instaService', function($http) {
         return {
             getFeed: function(id) {
-                return $http.post('http://localhost:3000/api/myphoto', {
+                return $http.post('http://localhost:3000/users/myphoto', {
                     instagram_id: id
                 });
             },
             getMediaLocation: function(id, lat, lng, d) {
-                return $http.post('http://localhost:3000/api/locationPhoto', {
+                return $http.post('http://localhost:3000/users/locationPhoto', {
                     instagram_id: id,
                     lat: lat,
                     lng: lng,
